27f14fbfaecd7bbc6d19f2f8f7c2b53b82686f05
Anuitex-1 <Anuitex-1@DESKTOP-IIQACD4>
2017-08-10 17:57:29 +0300

    Magazine

---
+++ Library.DataEF/Repositories/MagazineRepository.cs
@@ -0,0 +1,18 @@
+﻿using Library.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataEF.Repositories
+{
+    public class MagazineRepository : Entities.BaseRepository<Magazine>
+    {
+        public MagazineRepository(ApplicationContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
--- Library.Web/Controllers/MagazineController.cs
+++ Library.Web/Controllers/MagazineController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Library.DataEF;
+using Library.DataEF.Repositories;
+using Library.Entities;
+using Library.ViewModels.MagazineViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +13,77 @@ namespace Library.Web.Controllers
     public class MagazineController : Controller
     {
         // GET: Magazine
+        ApplicationContext _applicationContext;
+        MagazineRepository _magazineRepository;
+        public MagazineController()
+        {
+            _applicationContext = new ApplicationContext();
+            _magazineRepository = new MagazineRepository(_applicationContext);
+
+        }
+        // GET: Magazine
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult GetMagazines()
+        {
+            var Magazines = _magazineRepository.Get().ToList();
+            return Json(Magazines, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(AddMagazineViewModel view)
+        {
+            if (view == null)
+            {
+                return RedirectToAction("Index", "Magazine");
+            }
+            var magazineNew = new Magazine();
+
+            magazineNew.Author = view.Author;
+            magazineNew.Name = view.Name;
+            magazineNew.MagazineNumber = view.MagazineNumber;
+            _magazineRepository.Insert(magazineNew);
+            _magazineRepository.Save();
+            return RedirectToAction("Index", "Magazine");
+        }
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return RedirectToAction("Index", "Magazine");
+            }
+            var view = new EditMagazineViewModel();
+            var magazine = _magazineRepository.GetByID(id);
+            view.Author = magazine.Author;
+            view.Name = magazine.Name;
+            view.MagazineNumber = magazine.MagazineNumber;
+            return View(view);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditMagazineViewModel view)
+        {
+            var magazine = _magazineRepository.GetByID(view.Id);
+            magazine.Author = view.Author;
+            magazine.Name = view.Name;
+            magazine.MagazineNumber = view.MagazineNumber;
+            _magazineRepository.Update(magazine);
+            _magazineRepository.Save();
+            return RedirectToAction("Index", "Magazine");
+        }
+        public ActionResult Delete(string id)
+        {
+            _magazineRepository.Delete(id);
+            _magazineRepository.Save();
+            return RedirectToAction("Index", "Magazine");
         }
     }
 }
