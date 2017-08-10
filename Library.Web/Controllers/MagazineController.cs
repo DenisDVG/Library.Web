a159eb6a2b94eac4894bf96f31ab34915e7c930f
Anuitex-1 <Anuitex-1@DESKTOP-IIQACD4>
2017-08-10 17:37:27 +0300

    Magazine

---
+++ Library.Web/Controllers/MagazineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Web.Controllers
+{
+    public class MagazineController : Controller
+    {
+        // GET: Magazine
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
