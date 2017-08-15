19fa7e5ce8a9bd33de25f152fe984a359e27d769
Ihor Shumakov <igorshumakov04@gmail.com>
2017-08-15 16:12:43 +0300

    update

--- Library.Entities/Brochure.cs
+++ Library.Entities/Brochure.cs
@@ -9,7 +9,7 @@ namespace Library.Entities
 {
     public class Brochure : BaseEntity
     {
-        public int TomFiling { get; set; }
+        public int TomFilling { get; set; }
         public CoverType CoverType { get; set; }
         public string NumberPages { get; set; }
         public DateTime? PublishingYear { get; set; }
