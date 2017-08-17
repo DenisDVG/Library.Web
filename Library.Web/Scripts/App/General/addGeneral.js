1471f7e93594aeed8eba14eb418b772a236c9927
Anuitex-1 <Anuitex-1@DESKTOP-IIQACD4>
2017-08-17 13:37:27 +0300

    setValidateClass

--- Library.Web/Scripts/App/General/addGeneral.js
+++ Library.Web/Scripts/App/General/addGeneral.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿var validete;
+$(document).ready(function () {
 
     fillFilds();
     $("ul.nav-sidebar li").each(function () {
@@ -99,59 +100,130 @@
     });
     $("#typePublication").change(function () {
         setShowInputs();
+        setValidateClass();
     });
 
 })
 
-function validete() {
+validete = function () {
+    var isvalidate = true;
     var isPublishingYear = isYearofBirthValid($("#PublishingYear").val());
-    $("#btnTypeSubmit").click();
+    var isPublicationName = isEmpty($("#PublicationName").val());
+    var lementsInput = document.querySelectorAll('input');
+    console.log(lementsInput);
+
+    for (var i = 0; i < lementsInput.length; i++) {
+        if (!isEmpty(lementsInput[i].value))
+        {
+            lementsInput[i].className = lementsInput[i].className.replace(/errorClass/, "");
+            lementsInput[i].className += " errorClass";
+            if (!lementsInput[i].classList.contains('notValidate'))
+            {
+                isvalidate = false;
+            }
+        }
+        if (isEmpty(lementsInput[i].value)) {
+            lementsInput[i].className = lementsInput[i].className.replace(/errorClass/, "");
+        }
+    }
+    if (!isPublishingYear)
+    {
+        document.getElementById("PublishingYear").classList.remove('errorClass');
+        document.getElementById("PublishingYear").classList.add('errorClass');
+    }
+    if (isPublishingYear) {
+        document.getElementById("PublishingYear").classList.remove('errorClass');
+
+    }
+
+    if (isvalidate && isPublishingYear)
+    {
+        $("#btnTypeSubmit").click();
+    }
+    
     
 }
 
+function setValidateClass()
+{
+    var lementsInput = document.querySelectorAll('input');
+    for (var i = 0; i < lementsInput.length; i++) {
+
+
+
+        if ($("#typePublication").val() == "1") { // Book
+            if (lementsInput[i].id == "Author" || lementsInput[i].id == "NumberPages" || lementsInput[i].id == "PublishingYear") {
+                lementsInput[i].classList.remove('notValidate');
+            }
+            if (lementsInput[i].id == "MagazineNumber" || lementsInput[i].id == "PublicationDate" || lementsInput[i].id == "TomFiling") {
+                lementsInput[i].classList.add('notValidate');
+            }
+        }
+        if ($("#typePublication").val() == "2") { // Magazine
+            if (lementsInput[i].id == "MagazineNumber" || lementsInput[i].id == "PublicationDate") {
+                lementsInput[i].classList.remove('notValidate');
+            }
+            if (lementsInput[i].id == "Author" || lementsInput[i].id == "NumberPages" || lementsInput[i].id == "PublishingYear" || lementsInput[i].id == "TomFiling") {
+                lementsInput[i].classList.add('notValidate');
+            }
+        }
+        if ($("#typePublication").val() == "3") { // Magazine
+            if (lementsInput[i].id == "NumberPages" || lementsInput[i].id == "PublishingYear"|| lementsInput[i].id == "PublicationDate" || lementsInput[i].id == "TomFiling") {
+                lementsInput[i].classList.remove('notValidate');
+            }
+            if (lementsInput[i].id == "Author" || lementsInput[i].id == "MagazineNumber") {
+                lementsInput[i].classList.add('notValidate');
+            }
+        }
+    }
+}
+
 function setShowInputs() {
+    if ($("#typePublication").val() == "0") // None
+    {
+        $(".brochureShow").each(function () {
+            $(this).hide();
+        });
+        $(".magazineShow").each(function () {
+            $(this).hide();
+        });
+        $(".bookShow").each(function () {
+            $(this).hide();
+        });
+    }
     if ($("#typePublication").val() == "1") // Book
     {
         $(".brochureShow").each(function () {
             $(this).hide();
-            $(this).removeAttr("required");
         });
         $(".magazineShow").each(function () {
             $(this).hide();
-            $(this).removeAttr("required");
         });
         $(".bookShow").each(function () {
-            $(this).attr('required', 'required');
             $(this).show();
         });
     }
     if ($("#typePublication").val() == "2") // Magazine
     {
         $(".bookShow").each(function () {
-            $(this).removeAttr("required");
             $(this).hide();
         });
         $(".brochureShow").each(function () {
-            $(this).removeAttr("required");
             $(this).hide();
         });
         $(".magazineShow").each(function () {
-            $(this).attr('required', 'required');
             $(this).show();
         });
     }
     if ($("#typePublication").val() == "3") // Brochure
     {
         $(".bookShow").each(function () {
-            $(this).removeAttr("required");
             $(this).hide();
         });
         $(".magazineShow").each(function () {
-            $(this).removeAttr("required");
             $(this).hide();
         });
         $(".brochureShow").each(function () {
-            $(this).attr('required', 'required');
             $(this).show();
         });
     }
@@ -162,7 +234,13 @@ function setShowInputs() {
 
 function isYearofBirthValid(birth) {
     if (parseInt(birth) < 1400 || parseInt(birth) > new Date().getFullYear() + 1) {
-        alert("invalid year");
+        return false;
+    }
+    return true;
+}
+
+function isEmpty(value) {
+    if (value == null || value == "") {
         return false;
     }
     return true;
@@ -170,17 +248,16 @@ function isYearofBirthValid(birth) {
 
 function fillFilds() {
 
-    $("input:text").each(function () {
-        $(this).val(" ");
-    });
-    $("input[type=number]").each(function () {
-        $(this).val(0);
-    });
+    //$("input:text").each(function () {
+    //    $(this).val(" ");
+    //});
+    //$("input[type=number]").each(function () {
+    //    $(this).val(0);
+    //});
     var currdate = new Date();
     var currdate = (currdate.getMonth() + 1) + '/' + currdate.getDate() + '/' + currdate.getFullYear();
     var currdateOnlyYear = new Date();
     var currdateOnlyYear = currdateOnlyYear.getFullYear();
     $("#PublishingYear").val(currdateOnlyYear);
     $("#PublicationDate").val(currdate);
-
 }
