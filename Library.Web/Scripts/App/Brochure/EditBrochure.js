04021a578ba5509570fd9a3c77df607c1e9d5a3d
Anuitex-1 <Anuitex-1@DESKTOP-IIQACD4>
2017-08-18 10:41:29 +0300

    Brochure

--- Library.Web/Scripts/App/Brochure/EditBrochure.js
+++ Library.Web/Scripts/App/Brochure/EditBrochure.js
@@ -3,6 +3,9 @@
 var valueEdit;
 
 $(document).ready(function () {
+    $("#btnTypeButton").click(function () {
+        validete();
+    });
     $("#PublishingHousesIds").val("Error");
     
 
@@ -82,4 +85,64 @@ function setValue() {
     }
     console.log(iDs);
     $("#PublishingHousesIds").val(iDs);
+}
+
+
+function setValue() {
+    var multiselect = $("#orders").data("kendoMultiSelect");
+    var dataItem = multiselect.dataItems();
+    var iDs = [];
+    for (var i = 0; i < dataItem.length; i++) {
+        iDs.push(dataItem[i].Id);
+    }
+    console.log(iDs);
+    $("#PublishingHousesIds").val(iDs);
+}
+
+function validete() {
+    var isvalidate = true;
+    var isPublishingYear = isYearofBirthValid($("#PublishingYear").val());
+    var isPublicationName = isEmpty($("#PublicationName").val());
+    var lementsInput = document.querySelectorAll('input');
+    console.log(lementsInput);
+
+    for (var i = 0; i < lementsInput.length; i++) {
+        if (!isEmpty(lementsInput[i].value)) {
+            lementsInput[i].className = lementsInput[i].className.replace(/errorClass/, "");
+            lementsInput[i].className += " errorClass";
+            //k-input
+            if (!lementsInput[i].classList.contains("k-input")) {
+                isvalidate = false;
+            }
+        }
+        if (isEmpty(lementsInput[i].value)) {
+            lementsInput[i].className = lementsInput[i].className.replace(/errorClass/, "");
+        }
+    }
+    if (!isPublishingYear) {
+        document.getElementById("PublishingYear").classList.remove('errorClass');
+        document.getElementById("PublishingYear").classList.add('errorClass');
+    }
+    if (isPublishingYear) {
+        document.getElementById("PublishingYear").classList.remove('errorClass');
+    }
+
+    if (isvalidate && isPublishingYear) {
+        $("#btnTypeSubmit").click();
+    }
+}
+
+
+function isEmpty(value) {
+    if (value == null || value == "") {
+        return false;
+    }
+    return true;
+}
+
+function isYearofBirthValid(birth) {
+    if (parseInt(birth) < 1400 || parseInt(birth) > new Date().getFullYear() + 1 || birth == "") {
+        return false;
+    }
+    return true;
 }
