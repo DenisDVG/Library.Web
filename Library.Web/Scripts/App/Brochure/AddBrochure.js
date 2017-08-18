81f462cd1341927edca3c86e2756408419b1cce7
Anuitex-1 <Anuitex-1@DESKTOP-IIQACD4>
2017-08-18 10:28:46 +0300

    Brochure Add

--- Library.Web/Scripts/App/Brochure/AddBrochure.js
+++ Library.Web/Scripts/App/Brochure/AddBrochure.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function () {
-
+    $("#btnTypeButton").click(function () {
+        validete();
+    });
     $("#PublishingHousesIds").val("Error");
     $("#orders").kendoMultiSelect({
         placeholder: "Select Publishing Houses...",
@@ -52,3 +54,49 @@
 
 
 })
+
+
+function validete() {
+    var isvalidate = true;
+    var isPublishingYear = isYearofBirthValid($("#PublishingYear").val());
+    var isPublicationName = isEmpty($("#PublicationName").val());
+    var lementsInput = document.querySelectorAll('input');
+    console.log(lementsInput);
+
+    for (var i = 0; i < lementsInput.length; i++) {
+        if (!isEmpty(lementsInput[i].value)) {
+            lementsInput[i].className = lementsInput[i].className.replace(/errorClass/, "");
+            lementsInput[i].className += " errorClass";
+            isvalidate = false;
+        }
+        if (isEmpty(lementsInput[i].value)) {
+            lementsInput[i].className = lementsInput[i].className.replace(/errorClass/, "");
+        }
+    }
+
+    if (!isPublishingYear) {
+        document.getElementById("PublishingYear").classList.remove('errorClass');
+        document.getElementById("PublishingYear").classList.add('errorClass');
+    }
+    if (isPublishingYear) {
+        document.getElementById("PublishingYear").classList.remove('errorClass');
+    }
+
+    if (isvalidate && isPublishingYear) {
+        $("#btnTypeSubmit").click();
+    }
+
+
+}
+function isEmpty(value) {
+    if (value == null || value == "") {
+        return false;
+    }
+    return true;
+}
+function isYearofBirthValid(birth) {
+    if (parseInt(birth) < 1400 || parseInt(birth) > new Date().getFullYear() + 1 || birth == "") {
+        return false;
+    }
+    return true;
+}
